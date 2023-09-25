ff0621e551193842c42c33c4499d7514a32b54f8
jep <n.siomusic@gmail.com>
2023-09-25 20:57:37 +0700

    update: Tugas 4 Progress

--- main/views.py
+++ main/views.py
@@ -1,24 +1,27 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from main.models import Item
-from django.http import HttpResponseRedirect
+from django.http import HttpResponseRedirect, HttpResponse
 from main.forms import ItemForm
 from django.urls import reverse
-from django.http import HttpResponse
 from django.core import serializers
-
-# Mengembalikan Data dalam Bentuk XML
-
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib import messages 
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.decorators import login_required
+import datetime
 
 # Create your views here.
+@login_required(login_url='/login')
 def show_main(request):
     context = {
         'app_name': 'The Sentimental Sylladex',
-        'name': 'Alma Putri Nashrida',
-        'pbp_class' : 'PBP E'
+        'name': request.user.username,
+        'pbp_class' : 'PBP E',
+        'last_login': request.COOKIES['last_login'],
     }
 
-    items = Item.objects.all() # changed .values()
-    amounts = Item.objects.all().values_list('amount')
+    items = Item.objects.filter(user=request.user) # changed .values()
+    amounts = Item.objects.filter(user=request.user).values_list('amount')
     amount_total = 0
     for amount in amounts:
         amount_total += int(amount[0])
@@ -34,7 +37,9 @@ def create_item(request):
     form = ItemForm(request.POST or None)
 
     if form.is_valid() and request.method == "POST":
-        form.save()
+        item = form.save(commit=False)
+        item.user = request.user
+        item.save()
         return HttpResponseRedirect(reverse('main:show_main'))
 
     context['form'] = form
@@ -75,3 +80,48 @@ def show_xml_by_id(request, id):
 def show_json_by_id(request, id):
     data = Item.objects.filter(pk=id)
     return HttpResponse(serializers.serialize("json", data), content_type="application/json")
+
+def register(request):
+    form = UserCreationForm()
+
+    if request.method == "POST":
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Your account has been successfully created!')
+            return redirect('main:login')
+    context = {'form':form}
+    return render(request, 'register.html', context)
+
+def login_user(request):
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            response = HttpResponseRedirect(reverse("main:show_main")) 
+            response.set_cookie('last_login', str(datetime.datetime.now()))
+            return response
+        else:
+            messages.info(request, 'Sorry, incorrect username or password. Please try again.')
+    context = {}
+    return render(request, 'login.html', context)
+
+def logout_user(request):
+    logout(request)
+    response = HttpResponseRedirect(reverse('main:login'))
+    response.delete_cookie('last_login')
+    return response
+
+# def adjust_amount(request, id, num):
+#     data = Item.objects.filter(pk=id)
+
+#     if ((data.amount + num) <= 0):
+#         data.delete()
+#     else:
+#         data.amount += num
+#         data.save()
+
+#     return redirect('main:main')
+#     # is this right idk what else to do
